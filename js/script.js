19a67704458933dd4129af71ca3bcdf42af941e1
Joao Moutinho <joao.moutinho@gmail.com>
2020-04-22 23:48:38 +0100

    v2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 function nada() {
     window.alert('Isto ainda não faz nada');
 }
+
+function expandir_encomenda() {
+    this.innerHTML = "-";
+    console.log(this.parentElement.nextElementSibling.nextElementSibling);
+    var container_artigos = this.parentElement.nextElementSibling.nextElementSibling;
+
+    container_artigos.style.cssText = "height: 200px";
+
+}
+
+var lista_botoes_encomenda = document.querySelectorAll(".botao-encomenda");
+
+for (let i = 0; i < lista_botoes_encomenda.length; i++) {
+    lista_botoes_encomenda[i].onclick = expandir_encomenda;
+}
